8359a670edb00b4e2763fa51a939da78b597510f
Kazuya Tsubone <kazuya.tsubone@nulab-inc.com>
2018-08-13 13:08:55 +0900

    nuxt example

---
+++ pages/todos/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <input type="checkbox" :checked="todo.done" @change="toggle(todo)">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">remove</button>
+    </li>
+    <li><input placeholder="What needs to be done?" @keyup.enter="addTodo"></li>
+  </ul>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  computed: {
+    todos () { return this.$store.state.todos.list }
+  },
+  methods: {
+    removeTodo (todo) {
+      console.log(todo);
+      this.$store.commit('todos/remove', todo);
+    },
+    addTodo (e) {
+      this.$store.commit('todos/add', e.target.value)
+      e.target.value = ''
+    },
+    ...mapMutations({
+      toggle: 'todos/toggle'
+    })
+  }
+}
+</script>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
